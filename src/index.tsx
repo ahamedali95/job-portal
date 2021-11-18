590093f1093fb23e4dccb1e1bec459208a8d1a86
Ahamed <ahamedali95@gmail.com>
2021-11-18 09:54:14 -0500

    JOB-3: initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,26 +3,13 @@ import './wdyr';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from 'react-redux';
-
-import {ThemeProvider} from '@material-ui/core/styles';
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-
-import DateFnsUtils from '@date-io/date-fns';
-
 import * as serviceWorker from './service-worker';
-import { App } from './pages/App';
-import theme from './layout/theme';
-
-import './styles/index.scss';
+import App from './App';
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <App />
-    </MuiPickersUtilsProvider>
-  </ThemeProvider>,
-  document.getElementById('root'));
+    <App />,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
